eb392213a4dd0ac12cedbb5c20932ee6df3bf5b0
priyesh rai <jagritip134@gmail.com>
2026-09-03 17:23:36 +0530

    Gofmt non-Windows WireGuard stub

--- internal/wireguard/manager_stub.go
+++ internal/wireguard/manager_stub.go
@@ -4,10 +4,22 @@ package wireguard
 
 import (
 	"errors"
+
 	"github.com/ujjwalelectricals/VPN/internal/model"
 )
 
-func InstallAndStart(_ string, _ model.Node) error { return errors.New("VPN tunnel management is currently supported on Windows only") }
-func Stop(_ model.Node) error { return errors.New("VPN tunnel management is currently supported on Windows only") }
-func Uninstall(_ model.Node) error { return errors.New("VPN tunnel management is currently supported on Windows only") }
-func Status(_ model.Node) model.Status { return model.Status{Installed:false, Message:"Windows-only WireGuard manager"} }
+func InstallAndStart(_ string, _ model.Node) error {
+	return errors.New("VPN tunnel management is currently supported on Windows only")
+}
+
+func Stop(_ model.Node) error {
+	return errors.New("VPN tunnel management is currently supported on Windows only")
+}
+
+func Uninstall(_ model.Node) error {
+	return errors.New("VPN tunnel management is currently supported on Windows only")
+}
+
+func Status(_ model.Node) model.Status {
+	return model.Status{Installed: false, Message: "Windows-only WireGuard manager"}
+}
